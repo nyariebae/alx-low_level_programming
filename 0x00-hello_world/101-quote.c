df511b88e269ef79e65cfaf1464e38a8da679ced
Talent <talentnyaradzo@outlook.com>
2022-09-09 13:47:00 -0700

    everything

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 
 
 /**
+<<<<<<< HEAD
 
  * main - Entry point
 
@@ -12,9 +13,16 @@
 
  */
 
+=======
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+>>>>>>> a6431b7ad65cc32a2e0eb5470863b9796b57269c
 int main(void)
   
 {
+<<<<<<< HEAD
   
   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
   
@@ -22,4 +30,8 @@ int main(void)
   
   return (1);
   
+=======
+fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+return (1);
+>>>>>>> a6431b7ad65cc32a2e0eb5470863b9796b57269c
 }
