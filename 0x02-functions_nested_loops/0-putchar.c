6499b9085a978f9e1ca7fdc8955decd78900b2b0
Talent <talentnyaradzo@outlook.com>
2022-09-14 19:00:42 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - prints _putchar, followed by a new line
+typedef int make_iso_compilers_happy;
+
+ *
+
+ * Description: prints _putchar
+
+ *
+
+ * Return: 0 (Success)
+
+ */
+int 0-putchar
+*typedef int make_iso_compilers_happy;
+
+
+int main(void)
+  
+{
+  
+  char text[9] = "_putchar";
+  
+  int i = 0;
+  
+
+  
+  for (i = 0; i < 8; i++)
+    
+    {
+      
+      _putchar(text[i]);
+      
+    }
+  
+  _putchar('\n');
+  
+
+  
+  return (0);
+  
+}
+
