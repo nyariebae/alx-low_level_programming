2b65282133ada0744a8a4a4ddc28eff6463e52ce
nyariebae <111093808+nyariebae@users.noreply.github.com>
2022-09-21 05:12:14 +0200

    Update 3-main.c

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,31 +1,14 @@
 #include "main.h"
-
-
-
 /**
-
- * _puts - prints a string, followed by a new line to stdout
-
- * @str: pointer to the string to print
-
- */
-
+* _puts - prints a string, followed by a new line to stdout
+* @str: pointer to the string to print
+*/
 void _puts(char *str)
-  
 {
-  
-  int i;
-  
-
-  
-  for (i = 0; str[i] != '\0'; i++)
-    
-    {
-      
-      _putchar(str[i]);
-      
-    }
-  
-  _putchar(10);
-  
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar(10);
 }
