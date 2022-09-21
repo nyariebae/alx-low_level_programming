846dd87556ff2a470e4a7fe89eb77c6371d4f91d
Talent <talentnyaradzo@outlook.com>
2022-09-20 19:22:47 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: pointer to the first value
+ * @b: pointer to the second value
+ */
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
+
