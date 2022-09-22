20d2ea3aec16e53cdb6ed519973bbaf890f633bf
Talent <talentnyaradzo@outlook.com>
2022-09-21 17:22:32 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,49 @@
 #include "main.h"
+
+
+
 /**
-* rev_string -  reverses a string
-* @s: string to be reversed
-*/
-void rev_string(char *s)
+
+ * puts2 - Prints every other character of a string
+
+ * @str: The string to be treated
+
+ *
+
+ * Return: void
+
+ */
+
+void puts2(char *str)
+  
 {
-char tmp;
-int i, len, len1;
-len = 0;
-len1 = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-len1 = len - 1;
-for (i = 0; i < len / 2; i++)
-{
-tmp = s[i];
-s[i] = s[len1];
-s[len1--] = tmp;
-}
+  
+  int i;
+  
+  int j = 0;
+  
+
+  
+  while (str[j] != '\0')
+    
+    {
+      
+      j++;
+      
+    }
+  
+
+  
+  for (i = 0; i < j; i += 2)
+    
+    {
+      
+      _putchar(str[i]);
+      
+    }
+  
+
+  
+  _putchar('\n');
+  
 }
