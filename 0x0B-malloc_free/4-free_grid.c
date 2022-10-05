f4a2328911a4758ebe1ed9fce0f4755e7cbc446f
nyariebae <111093808+nyariebae@users.noreply.github.com>
2022-10-05 03:40:02 +0200

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,35 +1,16 @@
 #include "main.h"
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
-
-
 /**
-
- * free_grid - freeing matrix
-
- * @grid: matrix being passed
-
- * @height: freeing using this variable
-
- * Return: nothing
-
- */
-
+* free_grid - freeing matrix
+* @grid: matrix being passed
+* @height: freeing using this variable
+* Return: nothing
+*/
 void free_grid(int **grid, int height)
-  
 {
-  
-  int a;
-  
-
-  
-  for (a = 0; a < height; a++)
-    
-    free(grid[a]);
-  
-  free(grid);
-  
+int a;
+for (a = 0; a < height; a++)
+free(grid[a]);
+free(grid);
 }
