33e4ae9a884b61f6308ab76c50dd408431fe0b1f
Talent <talentnyaradzo@outlook.com>
2022-10-05 17:38:34 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+
+ * _isupper - checks for uppercase character.
+
+ * @c: c is a Variable
+
+ * Return: Always 0.
+
+ */
+
+int _isupper(int c)
+  
+{
+  
+  if (c >= 65 && c <= 90)
+    
+    {
+      
+      return (1);
+      
+    }
+  
+  return (0);
+  
+}
