c84595aa47762151d758547ad038fc588cb6815d
nyariebae <111093808+nyariebae@users.noreply.github.com>
2022-10-12 01:51:36 +0200

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main -> prints the name of the file
-
- * Return: 0(on Success)
-
- */
-
-
-
+* main -> prints the name of the file
+* Return: 0(on Success)
+*/
 int main(void)
-  
 {
-  
-  printf("%s\n", __FILE__);
-  
-  return (0);
-  
+printf("%s\n", __FILE__);
+return (0);
 }
